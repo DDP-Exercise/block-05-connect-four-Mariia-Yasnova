6ec1ce73b5fc551dea95417e6f46911407130389
Yasnovaaa <s2510892037@fhooe.at>
2026-05-12 01:31:34 +0200

    fertig

--- js/model.connectfour.js
+++ js/model.connectfour.js
@@ -1,39 +1,90 @@
 "use strict";
 
-//TODO: Think of this model as the game-logic.
-//      The model knows everything that is neccessary to manage
-//      the game. It knows the players, know who's turn it is,
-//      knows all the stones and where they are, knows if the
-//      game is over and if so, why (draw or winner). It knows
-//      which stones are the winning stones. The model also has
-//      sovereignty over the battlefield.
-//      First step: Create your model-object with all the properties
-//      necessary to store that information.
+const ConnectFourModel = {
+    rows: 6,
+    cols: 7,
+    board: [],
+    currentPlayer: 1,
+    gameOver: false,
+    winningStones: [],
 
-//TODO: Prepare some customEvents. The model should dispatch events when
-//      - The Player Changes
-//      - A stone was inserted
-//      - The Game is over (Draw or Winner)
-//      Don't forget to give your events a namespace.
-//      For each customEvent, just make a >method< for your model-object,
-//      that, when called, dispatches the event. Nothing else should
-//      happen in those methods.
+    init() {
+        this.board = Array.from({ length: this.rows }, () => Array(this.cols).fill(0));
+        this.currentPlayer = 1;
+        this.gameOver = false;
+        this.winningStones = [];
 
+        this.dispatchBoardChanged();
+        this.dispatchPlayerChange();
+    },
 
-//TODO: Initiate the battlefield. Your model needs a representation of the
-//      battlefield as data (two-dimensional array). Obviously, there are
-//      no stones yet in the field.
+    insertStone(col) {
+        if (this.gameOver) return;
 
-//TODO: The model should offer a method to insert a stone at a given column.
-//      If the stone can be inserted, the model should insert the stone,
-//      dispatch an event to let the world know that the battlefield has changed
-//      and check if the game is over now.
-//      Hint: This method will be called later by your controller, when the
-//      user makes an according input.
+        for (let row = this.rows - 1; row >= 0; row--) {
+            if (this.board[row][col] === 0) {
+                this.board[row][col] = this.currentPlayer;
+                this.dispatchBoardChanged();
 
-//TODO: Methods to check if the game is over, either by draw or a win.
-//      Let the world know in both cases what happend. If it's a win,
-//      Don't forget to store the winning stones and add this >detail<
-//      to your custom event.
+                if (this.checkWinner(row, col)) {
+                    this.gameOver = true;
+                    this.dispatchGameOver(`Player ${this.currentPlayer} Wins!`);
+                    return;
+                }
 
-//TODO: Method to change the current player (and dispatch the according event).
+                if (this.checkDraw()) {
+                    this.gameOver = true;
+                    this.dispatchGameOver("It's a Draw!");
+                    return;
+                }
+
+                this.changePlayer();
+                return;
+            }
+        }
+    },
+
+    changePlayer() {
+        this.currentPlayer = this.currentPlayer === 1 ? 2 : 1;
+        this.dispatchPlayerChange();
+    },
+
+    checkDraw() {
+        return this.board.every(row => !row.includes(0));
+    },
+
+    checkWinner(row, col) {
+        const directions = [[0, 1], [1, 0], [1, 1], [1, -1]];
+        for (let [dr, dc] of directions) {
+            let count = 1;
+            let stones = [[row, col]];
+            for (let dir of [-1, 1]) {
+                let r = row + dr * dir;
+                let c = col + dc * dir;
+                while (r >= 0 && r < this.rows && c >= 0 && c < this.cols && this.board[r][c] === this.currentPlayer) {
+                    count++;
+                    stones.push([r, c]);
+                    r += dr * dir;
+                    c += dc * dir;
+                }
+            }
+            if (count >= 4) {
+                this.winningStones = stones;
+                return true;
+            }
+        }
+        return false;
+    },
+
+    dispatchPlayerChange() {
+        document.dispatchEvent(new CustomEvent("connectfour.playerchange", { detail: { currentPlayer: this.currentPlayer } }));
+    },
+
+    dispatchBoardChanged() {
+        document.dispatchEvent(new CustomEvent("connectfour.boardchanged", { detail: { board: this.board } }));
+    },
+
+    dispatchGameOver(message) {
+        document.dispatchEvent(new CustomEvent("connectfour.gameover", { detail: { message, winningStones: this.winningStones } }));
+    }
+};
--- js/view.polished.js
+++ js/view.polished.js
@@ -1,14 +1,44 @@
 "use strict";
 
-//TODO: Think of this view as your game board.
-//      Your view should listen to various custom events of your model.
-//      For each event of your model, there should be a clear visual
-//      representation of what's going on.
+const ConnectFourView = {
+    boardElement: null,
 
-//TODO: Update the field. Show the whole battlefield with all the stones
-//      that are already played.
+    init() {
+        this.boardElement = document.getElementById("board");
+    },
 
-//TODO: Show the current player
+    render(board) {
+        if (!this.boardElement) return;
+        this.boardElement.innerHTML = "";
 
-//TODO: Notify the player when the game is over. Make it clear how the
-//      Game ended. If it's a win, show the winning stones.
+        board.forEach((row, r) => {
+            row.forEach((cellValue, c) => {
+                const cell = document.createElement("div");
+                cell.classList.add("cell");
+                cell.dataset.col = c;
+                if (cellValue === 1) cell.classList.add("player1");
+                if (cellValue === 2) cell.classList.add("player2");
+                this.boardElement.appendChild(cell);
+            });
+        });
+    },
+
+    updateCurrentPlayer(player) {
+        const el = document.getElementById("current-player");
+        if (el) el.textContent = `Current Player: Player ${player}`;
+    },
+
+    showWinner(message, winningStones) {
+        document.getElementById("message").textContent = message;
+        const cells = document.querySelectorAll(".cell");
+        winningStones.forEach(([r, c]) => {
+            const index = r * 7 + c;
+            if (cells[index]) cells[index].classList.add("winner");
+        });
+    }
+};
+
+// Подписка на события
+document.addEventListener("connectfour.boardchanged", (e) => ConnectFourView.render(e.detail.board));
+document.addEventListener("connectfour.playerchange", (e) => ConnectFourView.updateCurrentPlayer(e.detail.currentPlayer));
+document.addEventListener("connectfour.gameover", (e) => ConnectFourView.showWinner(e.detail.message, e.detail.winningStones));
